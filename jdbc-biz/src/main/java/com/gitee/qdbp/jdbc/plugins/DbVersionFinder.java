a51f9fe245f10b73db607850878d420d95adfa84
zhaohuihua <zhaohuihua@126.com>
2019-06-06 23:31:19 +0800

    增加注释

--- jdbc-biz/src/main/java/com/gitee/qdbp/jdbc/plugins/DbVersionFinder.java
+++ jdbc-biz/src/main/java/com/gitee/qdbp/jdbc/plugins/DbVersionFinder.java
@@ -3,8 +3,14 @@ package com.gitee.qdbp.jdbc.plugins;
 import org.springframework.jdbc.core.JdbcOperations;
 import com.gitee.qdbp.jdbc.model.DbVersion;
 
+/**
+ * 数据库信息查询接口
+ *
+ * @author zhaohuihua
+ * @version 190606
+ */
 public interface DbVersionFinder {
-    
+
     DbVersion findDbVersion(JdbcOperations jdbcOperations);
 
 }
