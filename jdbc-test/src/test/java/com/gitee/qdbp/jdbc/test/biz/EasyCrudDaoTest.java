3ee347d6eb533ebfe5a250795e3b54c44a8e5e15
zhaohuihua <zhaohuihua@126.com>
2019-07-27 21:12:23 +0800

    测试用例

--- jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/biz/EasyCrudDaoTest.java
+++ jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/biz/EasyCrudDaoTest.java
@@ -5,8 +5,14 @@ import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import org.testng.Assert;
 import org.testng.annotations.Test;
+import com.gitee.qdbp.able.jdbc.condition.DbWhere;
 import com.gitee.qdbp.jdbc.api.CoreJdbcBoot;
+import com.gitee.qdbp.jdbc.api.EasyCrudDao;
 import com.gitee.qdbp.jdbc.model.DbVersion;
+import com.gitee.qdbp.jdbc.test.enums.AccountType;
+import com.gitee.qdbp.jdbc.test.model.UserCoreBean;
+import com.gitee.qdbp.jdbc.utils.ParseTools;
+import com.gitee.qdbp.tools.utils.JsonTools;
 
 @Test
 @ContextConfiguration(locations = { "classpath:settings/spring/spring.xml" })
@@ -20,5 +26,17 @@ public class EasyCrudDaoTest extends AbstractTestNGSpringContextTests {
         DbVersion version = coreJdbcBoot.findDbVersion();
         System.out.println(version);
         Assert.assertNotNull(version);
+    }
+
+    @Test
+    public void testBeanQuery() {
+        UserCoreBean bean = new UserCoreBean();
+        bean.setUserType(AccountType.ADMIN);
+        bean.setUserCode("super");
+        DbWhere where = ParseTools.parseWhereFromEntity(bean);
+        EasyCrudDao<UserCoreBean> dao = coreJdbcBoot.buildCrudDao(UserCoreBean.class);
+        UserCoreBean user = dao.find(where);
+        System.out.println(JsonTools.toLogString(user));
+        Assert.assertNotNull(user);
     }
 }
