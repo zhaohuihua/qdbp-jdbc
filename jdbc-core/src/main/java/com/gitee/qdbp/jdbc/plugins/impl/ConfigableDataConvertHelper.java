39270efeae4f6e21ef1ea435edd0b02558da1d34
zhaohuihua <zhaohuihua@126.com>
2019-09-11 08:23:15 +0800

    ConfigableDataConvertHelper

--- jdbc-core/src/main/java/com/gitee/qdbp/jdbc/plugins/impl/ConfigableDataConvertHelper.java
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/plugins/impl/ConfigableDataConvertHelper.java
@@ -14,10 +14,20 @@ import com.alibaba.fastjson.util.TypeUtils;
  */
 public class ConfigableDataConvertHelper extends SimpleDataConvertHelper {
 
+//    /** Spring的类型转换处理类 **/
+//    private ConversionService conversionService;
     /** 枚举是否默认使用ordinal: true=ordinal, false=name **/
     private boolean enumUseOrdinal = true;
     /** 枚举例外列表, 使用与enumUseOrdinal相反的设置 **/
     private Map<String, Void> enumEspecialList = new HashMap<>();
+    /** 对象(基本对象除外)转字符串是否默认使用JSON格式: true=使用JSON格式, false=object.toString() **/
+    private boolean objectToStringUseJson = true;
+    /** 对象转字符串的例外列表, 使用与objectToStringUseJson相反的设置 **/
+    private Map<String, Void> objectToStringEspecialList = new HashMap<>();
+//    /** 未知类型对象转换是否启用: true=执行objectToString转换, false=不转换直接返回对象(由JDBC处理) **/
+//    private boolean untypedObjectConvertEnabled = false;
+//    /** 未知类型对象转换的例外列表, 使用与untypedObjectConvertEnabled相反的设置 **/
+//    private Map<String, Void> untypedObjectConvertEspecialList = new HashMap<>();
 
     protected Object doEnumToDbValue(Enum<?> variable) {
         if (enumEspecialList.containsKey(variable.getClass().getName())) {
@@ -35,12 +45,20 @@ public class ConfigableDataConvertHelper extends SimpleDataConvertHelper {
         return TypeUtils.castToString(variable);
     }
 
+    protected Object doVariableToBinary(Object variable) {
+        return variable;
+    }
+
     protected Object doVariableToBlob(Object variable) {
-        return doObjectToDbValue(variable);
+        return variable;
     }
 
     protected Object doVariableToClob(Object variable) {
         return doVariableToString(variable);
+    }
+
+    protected Object doVariableToOtherTypedValue(int sqlType, Object variable) {
+        return variable;
     }
 
     /** 判断枚举是否默认使用ordinal: true=ordinal, false=name **/
@@ -87,6 +105,54 @@ public class ConfigableDataConvertHelper extends SimpleDataConvertHelper {
         if (enumEspecialList != null) {
             for (String item : enumEspecialList) {
                 this.enumEspecialList.put(item, null);
+            }
+        }
+    }
+
+    /** 判断对象(基本对象除外)转字符串是否默认使用JSON格式: true=使用JSON格式, false=object.toString() **/
+    public boolean isObjectToStringUseJson() {
+        return objectToStringUseJson;
+    }
+
+    /** 设置对象(基本对象除外)转字符串是否默认使用JSON格式: true=使用JSON格式, false=object.toString() **/
+    public void setObjectToStringUseJson(boolean objectToStringUseJson) {
+        this.objectToStringUseJson = objectToStringUseJson;
+    }
+
+    /** 获取对象转字符串例外列表 **/
+    public List<String> getObjectToStringEspecialList() {
+        return new ArrayList<>(objectToStringEspecialList.keySet());
+    }
+
+    /** 设置对象转字符串例外列表 **/
+    public void setObjectToStringEspecialList(List<String> objectToStringEspecialList) {
+        this.objectToStringEspecialList.clear();
+        this.addObjectToStringEspecialList(objectToStringEspecialList);
+    }
+
+    /** 增加对象转字符串例外列表 **/
+    public void addObjectToStringEspecialList(Class<?>... objectToStringEspecialList) {
+        if (objectToStringEspecialList != null) {
+            for (Class<?> item : objectToStringEspecialList) {
+                this.objectToStringEspecialList.put(item.getName(), null);
+            }
+        }
+    }
+
+    /** 增加对象转字符串例外列表 **/
+    public void addObjectToStringEspecialList(String... objectToStringEspecialList) {
+        if (objectToStringEspecialList != null) {
+            for (String item : objectToStringEspecialList) {
+                this.objectToStringEspecialList.put(item, null);
+            }
+        }
+    }
+
+    /** 增加对象转字符串例外列表 **/
+    public void addObjectToStringEspecialList(List<String> objectToStringEspecialList) {
+        if (objectToStringEspecialList != null) {
+            for (String item : objectToStringEspecialList) {
+                this.objectToStringEspecialList.put(item, null);
             }
         }
     }
