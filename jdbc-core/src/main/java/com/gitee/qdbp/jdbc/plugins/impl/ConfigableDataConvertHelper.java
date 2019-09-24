0e8df6ca17fc003d33527ebe5686192dba5b82c9
zhaohuihua <zhaohuihua@126.com>
2019-09-24 08:11:39 +0800

    实现数据转换配置类

--- jdbc-core/src/main/java/com/gitee/qdbp/jdbc/plugins/impl/ConfigableDataConvertHelper.java
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/plugins/impl/ConfigableDataConvertHelper.java
@@ -14,22 +14,22 @@ import com.alibaba.fastjson.util.TypeUtils;
  */
 public class ConfigableDataConvertHelper extends SimpleDataConvertHelper {
 
-//    /** Spring的类型转换处理类 **/
-//    private ConversionService conversionService;
+    //    /** Spring的类型转换处理类 **/
+    //    private ConversionService conversionService;
     /** 枚举是否默认使用ordinal: true=ordinal, false=name **/
     private boolean enumUseOrdinal = true;
     /** 枚举例外列表, 使用与enumUseOrdinal相反的设置 **/
     private Map<String, Void> enumEspecialList = new HashMap<>();
-    /** 对象(基本对象除外)转字符串是否默认使用JSON格式: true=使用JSON格式, false=object.toString() **/
+    /** 对象(基本对象除外)转字符串convertToString()是否默认使用JSON格式: true=使用JSON格式, false=object.toString() **/
     private boolean objectToStringUseJson = true;
     /** 对象转字符串的例外列表, 使用与objectToStringUseJson相反的设置 **/
     private Map<String, Void> objectToStringEspecialList = new HashMap<>();
-//    /** 未知类型对象转换是否启用: true=执行objectToString转换, false=不转换直接返回对象(由JDBC处理) **/
-//    private boolean untypedObjectConvertEnabled = false;
-//    /** 未知类型对象转换的例外列表, 使用与untypedObjectConvertEnabled相反的设置 **/
-//    private Map<String, Void> untypedObjectConvertEspecialList = new HashMap<>();
+    /** 未知类型对象转换是否启用: true=执行convertToString()转换, false=不转换直接返回对象(由JDBC处理) **/
+    private boolean untypedObjectConvertEnabled = false;
+    /** 未知类型对象转换的例外列表, 使用与untypedObjectConvertEnabled相反的设置 **/
+    private Map<String, Void> untypedObjectConvertEspecialList = new HashMap<>();
 
-    protected Object doEnumToDbValue(Enum<?> variable) {
+    protected Object convertEnumToDbValue(Enum<?> variable) {
         if (enumEspecialList.containsKey(variable.getClass().getName())) {
             return enumUseOrdinal ? variable.name() : variable.ordinal();
         } else {
@@ -37,20 +37,40 @@ public class ConfigableDataConvertHelper extends SimpleDataConvertHelper {
         }
     }
 
-    protected Object doObjectToDbValue(Object variable) {
-        return variable;
+    protected String convertObjectToString(Object variable) {
+        if (objectToStringEspecialList.containsKey(variable.getClass().getName())) {
+            return objectToStringUseJson ? variable.toString() : TypeUtils.castToString(variable);
+        } else {
+            return objectToStringUseJson ?  TypeUtils.castToString(variable) : variable.toString();
+        }
     }
 
-    protected String doVariableToString(Object variable) {
-        return TypeUtils.castToString(variable);
+    protected Object convertObjectToDbValue(Object variable) {
+        if (untypedObjectConvertEspecialList.containsKey(variable.getClass().getName())) {
+            return untypedObjectConvertEnabled ? variable : convertObjectToString(variable);
+        } else {
+            return untypedObjectConvertEnabled ?  convertObjectToString(variable) : variable;
+        }
+    }
+
+    protected Object doEnumToDbValue(Enum<?> variable) {
+        return convertEnumToDbValue(variable);
+    }
+
+    protected Object doObjectToDbValue(Object variable) {
+        return convertObjectToDbValue(variable);
+    }
+
+    protected Object doVariableToString(Object variable) {
+        return convertObjectToString(variable);
     }
 
     protected Object doVariableToBinary(Object variable) {
-        return variable;
+        return convertObjectToDbValue(variable);
     }
 
     protected Object doVariableToBlob(Object variable) {
-        return variable;
+        return convertObjectToDbValue(variable);
     }
 
     protected Object doVariableToClob(Object variable) {
@@ -58,7 +78,7 @@ public class ConfigableDataConvertHelper extends SimpleDataConvertHelper {
     }
 
     protected Object doVariableToOtherTypedValue(int sqlType, Object variable) {
-        return variable;
+        return convertObjectToDbValue(variable);
     }
 
     /** 判断枚举是否默认使用ordinal: true=ordinal, false=name **/
@@ -153,6 +173,54 @@ public class ConfigableDataConvertHelper extends SimpleDataConvertHelper {
         if (objectToStringEspecialList != null) {
             for (String item : objectToStringEspecialList) {
                 this.objectToStringEspecialList.put(item, null);
+            }
+        }
+    }
+
+    /** 判断未知类型对象转换是否启用: true=执行convertToString()转换, false=不转换直接返回对象(由JDBC处理) **/
+    public boolean isUntypedObjectConvertEnabled() {
+        return untypedObjectConvertEnabled;
+    }
+
+    /** 设置未知类型对象转换是否启用: true=执行convertToString()转换, false=不转换直接返回对象(由JDBC处理) **/
+    public void setUntypedObjectConvertEnabled(boolean untypedObjectConvertEnabled) {
+        this.untypedObjectConvertEnabled = untypedObjectConvertEnabled;
+    }
+
+    /** 获取类型对象转换的例外列表 **/
+    public List<String> getUntypedObjectConvertEspecialList() {
+        return new ArrayList<>(untypedObjectConvertEspecialList.keySet());
+    }
+
+    /** 设置类型对象转换的例外列表 **/
+    public void setUntypedObjectConvertEspecialList(List<String> untypedObjectConvertEspecialList) {
+        this.untypedObjectConvertEspecialList.clear();
+        this.addUntypedObjectConvertEspecialList(untypedObjectConvertEspecialList);
+    }
+
+    /** 增加类型对象转换的例外列表 **/
+    public void addUntypedObjectConvertEspecialList(Class<?>... untypedObjectConvertEspecialList) {
+        if (untypedObjectConvertEspecialList != null) {
+            for (Class<?> item : untypedObjectConvertEspecialList) {
+                this.untypedObjectConvertEspecialList.put(item.getName(), null);
+            }
+        }
+    }
+
+    /** 增加类型对象转换的例外列表 **/
+    public void addUntypedObjectConvertEspecialList(String... untypedObjectConvertEspecialList) {
+        if (untypedObjectConvertEspecialList != null) {
+            for (String item : untypedObjectConvertEspecialList) {
+                this.untypedObjectConvertEspecialList.put(item, null);
+            }
+        }
+    }
+
+    /** 增加类型对象转换的例外列表 **/
+    public void addUntypedObjectConvertEspecialList(List<String> untypedObjectConvertEspecialList) {
+        if (untypedObjectConvertEspecialList != null) {
+            for (String item : untypedObjectConvertEspecialList) {
+                this.untypedObjectConvertEspecialList.put(item, null);
             }
         }
     }
