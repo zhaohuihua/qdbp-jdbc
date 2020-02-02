36444b5bac56babde2165dc11a5861ee5ba0cecf
zhaohuihua <zhaohuihua@126.com>
2020-02-02 16:12:31 +0800

    修改测试用例

--- jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/biz/EasyCrudDaoTest.java
+++ jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/biz/EasyCrudDaoTest.java
@@ -11,8 +11,8 @@ import com.gitee.qdbp.able.jdbc.condition.DbWhere;
 import com.gitee.qdbp.jdbc.api.CoreJdbcBoot;
 import com.gitee.qdbp.jdbc.api.EasyCrudDao;
 import com.gitee.qdbp.jdbc.model.DbVersion;
-import com.gitee.qdbp.jdbc.test.enums.AccountType;
 import com.gitee.qdbp.jdbc.test.enums.UserState;
+import com.gitee.qdbp.jdbc.test.enums.UserType;
 import com.gitee.qdbp.jdbc.test.model.SysUserEntity;
 import com.gitee.qdbp.jdbc.utils.ParseTools;
 import com.gitee.qdbp.tools.utils.DateTools;
@@ -21,25 +21,25 @@ import com.gitee.qdbp.tools.utils.JsonTools;
 @Test
 @ContextConfiguration(locations = { "classpath:settings/spring/spring.xml" })
 public class EasyCrudDaoTest extends AbstractTestNGSpringContextTests {
-    
+
     private Logger log = LoggerFactory.getLogger(EasyCrudDaoTest.class);
 
     @Autowired
     private CoreJdbcBoot coreJdbcBoot;
 
-    @Test(priority = 1)
+    @Test(priority = 2)
     public void testVersionQuery() {
         DbVersion version = coreJdbcBoot.getSqlBufferJdbcOperations().findDbVersion();
         log.debug("DbVersion: {}", version);
         Assert.assertNotNull(version);
     }
 
-    @Test(priority = 2)
+    @Test(priority = 3)
     public void testUserBeanQuery() {
         SysUserEntity bean = new SysUserEntity();
         bean.setUserCode("super");
         bean.setSuperman(true);
-        bean.setUserType(AccountType.ADMIN);
+        bean.setUserType(UserType.ADMIN);
         bean.setUserState(UserState.NORMAL);
         DbWhere where = ParseTools.parseWhereFromEntity(bean);
         EasyCrudDao<SysUserEntity> dao = coreJdbcBoot.buildCrudDao(SysUserEntity.class);
@@ -48,12 +48,12 @@ public class EasyCrudDaoTest extends AbstractTestNGSpringContextTests {
         Assert.assertNotNull(user);
     }
 
-    @Test(priority = 3)
+    @Test(priority = 4)
     public void testUserWhereQuery() {
         DbWhere where = new DbWhere();
         where.on("userCode", "=", "super");
         where.on("superman", "=", true);
-        where.on("userType", "=", AccountType.ADMIN);
+        where.on("userType", "=", UserType.ADMIN);
         where.on("userState", "in", UserState.NORMAL, UserState.LOCKED);
         where.on("createTime", ">=", DateTools.parse("2017-01-01"));
         EasyCrudDao<SysUserEntity> dao = coreJdbcBoot.buildCrudDao(SysUserEntity.class);
