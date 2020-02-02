a7d053293007d5bd63aa479e03ba2f751ceef877
zhaohuihua <zhaohuihua@126.com>
2020-02-02 14:22:27 +0800

    MapToBeanConverter

---
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/plugins/MapToBeanConverter.java
@@ -0,0 +1,22 @@
+package com.gitee.qdbp.jdbc.plugins;
+
+import java.util.Map;
+
+/**
+ * Map到JavaBean的转换
+ *
+ * @author zhaohuihua
+ * @version 200201
+ */
+public interface MapToBeanConverter {
+
+    /**
+     * 将Map转换为Java对象
+     * 
+     * @param <T> 目标类型
+     * @param map Map
+     * @param clazz 目标Java类
+     * @return Java对象
+     */
+    <T> T mapToBean(Map<String, ?> map, Class<T> clazz);
+}
