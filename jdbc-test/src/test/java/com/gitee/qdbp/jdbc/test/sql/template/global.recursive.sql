ce3e79b33262f1c662ba89b7eafcd090edc07bc4
zhaohuihua <zhaohuihua@126.com>
2020-06-02 08:44:05 +0800

    测试SQL

---
+++ jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/sql/template/global.recursive.sql
@@ -0,0 +1,13 @@
+-- <<normalFindChildren>> 递归查询所有子节点
+-- 实测发现DB2不支持_开头的字段名, 不支持field AS alias, WITH AS中不支持INNER JOIN
+#{keyword} recursive_temp_table(temp_parent) AS (
+    SELECT #{codeField} temp_parent FROM #{tableName} WHERE #{codeField} IN ( ${startCodeCondition} ) 
+    UNION ALL
+    SELECT #{codeField} FROM #{tableName} A, recursive_temp_table B ON A.#{parentField} = B.temp_parent
+)
+SELECT #{selectFields} FROM #{tableName} WHERE #{codeField} IN (
+    SELECT temp_parent FROM recursive_temp_table
+)
+${'AND'whereCondition}
+#{'ORDER BY'orderByCondition}
+
---
+++ jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/sql/template/select.001.sql
@@ -0,0 +1,1 @@
+-- sql: user.test1 
