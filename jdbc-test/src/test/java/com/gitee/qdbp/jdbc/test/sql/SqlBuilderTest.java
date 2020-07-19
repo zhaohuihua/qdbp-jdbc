7272bcb870ed2e454271bca203f44fde82338df8
zhaohuihua <zhaohuihua@126.com>
2020-07-19 10:25:17 +0800

    SqlBuilder缩进测试

--- jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/sql/SqlBuilderTest.java
+++ jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/sql/SqlBuilderTest.java
@@ -2,6 +2,7 @@ package com.gitee.qdbp.jdbc.test.sql;
 
 import com.gitee.qdbp.jdbc.model.MainDbType;
 import com.gitee.qdbp.jdbc.plugins.SqlDialect;
+import com.gitee.qdbp.jdbc.sql.SqlBuffer;
 import com.gitee.qdbp.jdbc.sql.SqlBuilder;
 import com.gitee.qdbp.jdbc.utils.DbTools;
 import com.gitee.qdbp.tools.utils.DateTools;
@@ -10,7 +11,36 @@ public class SqlBuilderTest {
 
     public static void main(String[] args) {
         SqlDialect dialect = DbTools.buildSqlDialect(MainDbType.Oracle);
+        test1(dialect);
+        test2(dialect);
+    }
 
+    private static void test1(SqlDialect dialect) {
+        SqlBuilder buffer = new SqlBuilder();
+        buffer.ad("SELECT * FROM SYS_USER");
+        buffer.newline().tab();
+        buffer.ad("WHERE DEPT_CODE").ad('=').var("10001");
+        buffer.newline().tab();
+        buffer.ad("AND PHONE").ad(dialect.buildLikeSql("139%"));
+        buffer.ad("AND CREATE_USER").ad('=').var("zhh");
+        buffer.newline();
+        buffer.ad("AND CREATE_TIME", ">=").var(DateTools.parse("2019-01-01"));
+        buffer.newline();
+        buffer.ad("AND USER_STATE").ad('=').var(1);
+        buffer.ad("AND EFTFLAG IN").ad('(').var('E').ad(',').var('N').ad(')');
+        buffer.newline().tab(-2);
+        buffer.ad("UNION");
+        buffer.newline();
+        buffer.ad("SELECT * FROM SYS_USER");
+        buffer.newline().tab();
+        buffer.ad("WHERE DEPT_CODE").ad('=').var("10001");
+        buffer.ad("AND USER_STATE").ad('=').var(2);
+        buffer.ad("AND EFTFLAG IN").ad('(').var('E').ad(',').var('N').ad(')');
+
+        printSqlString(buffer.out(), dialect);
+    }
+
+    private static void test2(SqlDialect dialect) {
         SqlBuilder buffer = new SqlBuilder();
         buffer.ad("AND CREATE_USER").ad('=').var("zhh");
         buffer.newline();
@@ -31,9 +61,20 @@ public class SqlBuilderTest {
         selectBuffer.ad("SELECT * FROM SYS_USER").newline().tab();
         buffer.pd(selectBuffer);
 
-        System.out.println(buffer.end().getPreparedSqlString(dialect));
-        System.out.println(buffer.end().toString());
-        // String sql = buffer.end().getExecutableSqlString(dialect);
-        // System.out.println(DbTools.formatSql(sql, 0));
+        printSqlString(buffer.out(), dialect);
+    }
+
+    private static void printSqlString(SqlBuffer buffer, SqlDialect dialect) {
+        System.out.println("/******************************************************\\");
+        System.out.println("-- PreparedSqlString");
+        System.out.println(buffer.getPreparedSqlString(dialect));
+        System.out.println("--------------------------------------------------------");
+        System.out.println("-- ExecutableSqlString");
+        System.out.println(buffer.getExecutableSqlString(dialect));
+        System.out.println("--------------------------------------------------------");
+        System.out.println("-- LoggingSqlString");
+        System.out.println(buffer.getLoggingSqlString(dialect));
+        System.out.println("\\******************************************************/");
+        System.out.println();
     }
 }
