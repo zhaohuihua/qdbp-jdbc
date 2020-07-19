a5b101e57385df1fe614b2eb5e93df60f30823b0
zhaohuihua <zhaohuihua@126.com>
2020-07-19 16:27:23 +0800

    缩进优化

--- jdbc-core/src/main/java/com/gitee/qdbp/jdbc/plugins/impl/SimpleSqlFormatter.java
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/plugins/impl/SimpleSqlFormatter.java
@@ -1,10 +1,14 @@
 package com.gitee.qdbp.jdbc.plugins.impl;
 
-import java.util.regex.Pattern;
 import com.gitee.qdbp.jdbc.plugins.SqlFormatter;
+import com.gitee.qdbp.tools.utils.IndentTools;
 
-// 使用DruidSqlFormat格式化SQL
-// https://github.com/alibaba/druid/wiki/SQL_Format
+/**
+ * 简单的SQL格式化处理工具
+ *
+ * @author zhaohuihua
+ * @version 190601
+ */
 public class SimpleSqlFormatter implements SqlFormatter {
 
     public String format(String sql) {
@@ -12,39 +16,10 @@ public class SimpleSqlFormatter implements SqlFormatter {
     }
 
     public String format(String sql, int indent) {
-        String formatted = formatSql(sql);
-        String pretty = prettySql(formatted);
         if (indent <= 0) {
-            return pretty;
+            return sql;
         } else {
-            String tab = indent == 1 ? LEADING_SPACE : getIndentSpace(indent);
-            return tab + NEWLINE.matcher(pretty).replaceAll("\n" + tab);
+            return IndentTools.indentAll(sql, indent);
         }
     }
-
-    protected String getIndentSpace(int indent) {
-        StringBuilder buffer = new StringBuilder(indent);
-        for (int i = 0; i < indent; i++) {
-            buffer.append(LEADING_SPACE);
-        }
-        return buffer.toString();
-    }
-
-    private static Pattern LEADING_COMMA = Pattern.compile("(\\r?\\n\\s*)(,)(\\s*)");
-    private static Pattern TAB = Pattern.compile("\\t");
-    private static Pattern NEWLINE = Pattern.compile("\\n");
-    private static String LEADING_SPACE = "    ";
-
-    protected String formatSql(String sql) {
-        return sql;
-    }
-
-    protected String prettySql(String sql) {
-        // druid sql format的换行风格是换行后加前置逗号, 改为逗号后换行
-        sql = LEADING_COMMA.matcher(sql).replaceAll("$2$1");
-        // TAB替换为4个空格
-        sql = TAB.matcher(sql).replaceAll(LEADING_SPACE);
-        return sql;
-    }
-
 }
