83291b9947e80bf6450a13064adffb63c581b332
zhaohuihua <zhaohuihua@126.com>
2020-07-27 22:24:04 +0800

    EnumInterfaceConverter优化

--- jdbc-core/src/main/java/com/gitee/qdbp/jdbc/support/EnumInterfaceConverter.java
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/support/EnumInterfaceConverter.java
@@ -1,12 +1,16 @@
 package com.gitee.qdbp.jdbc.support;
 
+import java.math.BigInteger;
 import java.util.ArrayList;
 import java.util.HashSet;
 import java.util.List;
 import java.util.Set;
+import org.springframework.core.convert.ConversionFailedException;
 import org.springframework.core.convert.TypeDescriptor;
 import org.springframework.core.convert.converter.ConditionalGenericConverter;
 import com.gitee.qdbp.able.enums.EnumInterface;
+import com.gitee.qdbp.tools.utils.ConvertTools;
+import com.gitee.qdbp.tools.utils.StringTools;
 import com.gitee.qdbp.tools.utils.VerifyTools;
 
 /**
@@ -22,11 +26,11 @@ public class EnumInterfaceConverter<I extends EnumInterface, E extends Enum<E>> 
     private List<E> enumItems;
     /** 默认值(解析失败时的返回值) **/
     private I defaultValue;
+    /** 空值是否转换为默认值 **/
+    private boolean nullToDefault;
     private Class<I> interfaceType;
     private TypeDescriptor targetDescriptor;
     private Set<ConvertiblePair> convertiblePairs;
-    private static final TypeDescriptor STRING_TYPE = TypeDescriptor.valueOf(String.class);
-    private static final TypeDescriptor NUMBER_TYPE = TypeDescriptor.valueOf(Number.class);
 
     public EnumInterfaceConverter(Class<I> interfaceType, Class<E> enumType) {
         VerifyTools.requireNotBlank(interfaceType, "interfaceType");
@@ -48,18 +52,35 @@ public class EnumInterfaceConverter<I extends EnumInterface, E extends Enum<E>> 
     public EnumInterfaceConverter(Class<I> interfaceType, Class<E> enumType, String defaultValue) {
         this(interfaceType, enumType);
         this.defaultValue = convert(defaultValue);
+        this.nullToDefault = true;
     }
 
     public EnumInterfaceConverter(Class<I> interfaceType, Class<E> enumType, I defaultValue) {
         this(interfaceType, enumType);
         this.defaultValue = defaultValue;
+        this.nullToDefault = true;
+    }
+
+    public EnumInterfaceConverter(Class<I> interfaceType, Class<E> enumType, String defaultValue,
+            boolean nullToDefault) {
+        this(interfaceType, enumType);
+        this.defaultValue = convert(defaultValue);
+        this.nullToDefault = nullToDefault;
+    }
+
+    public EnumInterfaceConverter(Class<I> interfaceType, Class<E> enumType, I defaultValue, boolean nullToDefault) {
+        this(interfaceType, enumType);
+        this.defaultValue = defaultValue;
+        this.nullToDefault = nullToDefault;
     }
 
     private void initConvertiblePairs() {
         this.targetDescriptor = TypeDescriptor.valueOf(interfaceType);
         Set<ConvertiblePair> convertiblePairs = new HashSet<ConvertiblePair>(2);
         convertiblePairs.add(new ConvertiblePair(String.class, interfaceType));
-        convertiblePairs.add(new ConvertiblePair(Number.class, interfaceType));
+        convertiblePairs.add(new ConvertiblePair(Integer.class, interfaceType));
+        convertiblePairs.add(new ConvertiblePair(Long.class, interfaceType));
+        convertiblePairs.add(new ConvertiblePair(BigInteger.class, interfaceType));
         this.convertiblePairs = convertiblePairs;
     }
 
@@ -71,17 +92,55 @@ public class EnumInterfaceConverter<I extends EnumInterface, E extends Enum<E>> 
         }
     }
 
+    public I convert(Object source) {
+        if (source == null) {
+            return nullToDefault ? defaultValue : null;
+        }
+
+        if (source instanceof Integer || source instanceof Long || source instanceof BigInteger) {
+            return convert(((Number) source).intValue());
+        } else if (source instanceof String) {
+            String string = (String) source;
+            if (StringTools.isDigit(string)) {
+                return convert(ConvertTools.toInteger(string));
+            } else {
+                return convert(string);
+            }
+        } else {
+            TypeDescriptor sourceType = TypeDescriptor.forObject(source);
+            TypeDescriptor targetType = TypeDescriptor.valueOf(enumType);
+            throw new ConversionFailedException(sourceType, targetType, source, null);
+        }
+    }
+
     @SuppressWarnings("unchecked")
-    public I convert(Object type) {
-        if (type instanceof String || type instanceof Number) {
-            String target = type.toString();
-            for (E i : enumItems) {
-                if (i.name().equals(target) || String.valueOf(i.ordinal()).equals(target)) {
-                    return (I) i;
-                }
+    protected I convert(int source) {
+        for (E i : enumItems) {
+            if (i.ordinal() == source) {
+                return (I) i;
             }
         }
-        return (I) defaultValue;
+        return defaultValue;
+    }
+
+    @SuppressWarnings("unchecked")
+    protected I convert(String source) {
+        if (source.trim().length() == 0) {
+            return nullToDefault ? defaultValue : null;
+        }
+
+        for (E i : enumItems) {
+            if (i.name().equals(source)) {
+                return (I) i;
+            }
+        }
+        // 如果没有完全匹配的, 不区分大小写再遍历一次
+        for (E i : enumItems) {
+            if (i.name().equalsIgnoreCase(source)) {
+                return (I) i;
+            }
+        }
+        return defaultValue;
     }
 
     /** 获取接口类型 **/
@@ -99,6 +158,11 @@ public class EnumInterfaceConverter<I extends EnumInterface, E extends Enum<E>> 
         return defaultValue;
     }
 
+    /** 空值是否转换为默认值 **/
+    public boolean isNullToDefault() {
+        return nullToDefault;
+    }
+
     @Override
     public Set<ConvertiblePair> getConvertibleTypes() {
         return convertiblePairs;
@@ -106,8 +170,14 @@ public class EnumInterfaceConverter<I extends EnumInterface, E extends Enum<E>> 
 
     @Override
     public boolean matches(TypeDescriptor sourceType, TypeDescriptor targetType) {
-        return targetType.isAssignableTo(targetDescriptor)
-                && (sourceType.isAssignableTo(STRING_TYPE) || sourceType.isAssignableTo(NUMBER_TYPE));
+        if (targetType.isAssignableTo(targetDescriptor)) {
+            for (ConvertiblePair pair : convertiblePairs) {
+                if (sourceType.getObjectType().isAssignableFrom(pair.getSourceType())) {
+                    return true;
+                }
+            }
+        }
+        return false;
     }
 
     @Override
