fc3602b49eb35778b1b458db37b47f2ff2009f7f
zhaohuihua <zhaohuihua@126.com>
2020-07-27 22:28:17 +0800

    DataState转换类改为EnumExcludedToLastConverter

---
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/support/EnumExcludedToLastConverter.java
@@ -0,0 +1,19 @@
+package com.gitee.qdbp.jdbc.support;
+
+/**
+ * 枚举例外项转换为枚举最后一项的转换类
+ *
+ * @author zhaohuihua
+ * @version 20200726
+ */
+public class EnumExcludedToLastConverter<E extends Enum<E>> extends EnumExcludedToDefaultConverter<E> {
+
+    public EnumExcludedToLastConverter(Class<E> enumType) {
+        super(enumType, getEnumLastItem(enumType), false);
+    }
+
+    private static <E> E getEnumLastItem(Class<E> enumType) {
+        E[] enums = enumType.getEnumConstants();
+        return enums[enums.length - 1];
+    }
+}
