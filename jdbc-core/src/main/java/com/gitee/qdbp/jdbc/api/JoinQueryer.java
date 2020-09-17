16b073ed317b22decfb8cda01847ff60ddd9ba9e
zhaohuihua <zhaohuihua@126.com>
2020-09-17 08:25:00 +0800

    JoinQueryer增加方法

--- jdbc-core/src/main/java/com/gitee/qdbp/jdbc/api/JoinQueryer.java
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/api/JoinQueryer.java
@@ -4,6 +4,7 @@ import java.util.List;
 import java.util.Map;
 import com.gitee.qdbp.able.exception.ServiceException;
 import com.gitee.qdbp.able.jdbc.condition.DbWhere;
+import com.gitee.qdbp.able.jdbc.fields.Fields;
 import com.gitee.qdbp.able.jdbc.ordering.OrderPaging;
 import com.gitee.qdbp.able.jdbc.ordering.Orderings;
 import com.gitee.qdbp.able.jdbc.paging.PageList;
@@ -35,10 +36,25 @@ public interface JoinQueryer<T> {
      * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0
      * 
      * @param where 查询条件, 如果没有查询条件应传入DbWhere.NONE
-     * @param resultType 查询结果类型
      * @return 实体对象
      */
     T find(DbWhere where) throws ServiceException;
+
+    /**
+     * 根据查询条件获取对象<br>
+     * 注意: 默认查询条件由entityFieldFillExecutor添加, 只查有效项<br>
+     * SELECT {columnNames} FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0
+     * 
+     * @param fields 查询的字段: 全部字段传入Fields.ALL, 指定字段传入IncludeFields对象, 排除字段传入ExcludeFields对象<br>
+     *            如: new IncludeFields("a.id"), 将会查询A.ID<br>
+     *            如: new IncludeFields("id"), 将会查询A.ID,B.ID<br>
+     * @param where 查询条件, 如果没有查询条件应传入DbWhere.NONE
+     * @return 实体对象
+     */
+    T find(Fields fields, DbWhere where) throws ServiceException;
 
     /**
      * 查找所有的实体列表, 不分页<br>
@@ -48,7 +64,6 @@ public interface JoinQueryer<T> {
      * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
      * &nbsp;&nbsp;&nbsp;&nbsp;WHERE A.DATA_STATE=0
      * 
-     * @param resultType 查询结果类型
      * @return 列表数据
      */
     List<T> listAll() throws ServiceException;
@@ -59,13 +74,65 @@ public interface JoinQueryer<T> {
      * SELECT {columnNames} FROM {tableNameA} A<br>
      * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
      * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE A.DATA_STATE=0
+     * 
+     * @param fields 查询的字段: 全部字段传入Fields.ALL, 指定字段传入IncludeFields对象, 排除字段传入ExcludeFields对象<br>
+     *            如: new IncludeFields("a.id"), 将会查询A.ID<br>
+     *            如: new IncludeFields("id"), 将会查询A.ID,B.ID<br>
+     * @return 列表数据
+     */
+    List<T> listAll(Fields fields) throws ServiceException;
+
+    /**
+     * 查找所有的实体列表, 不分页<br>
+     * 注意: 默认查询条件由entityFieldFillExecutor添加, 只查有效项<br>
+     * SELECT {columnNames} FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
      * &nbsp;&nbsp;&nbsp;&nbsp;WHERE A.DATA_STATE=0<br>
      * &nbsp;&nbsp;&nbsp;&nbsp;ORDER BY {orderByConditions}
      * 
-     * @param orderings 排序字段
+     * @param orderings 排序字段, 不需要排序时应传入Orderings.NONE
      * @return 列表数据
      */
     List<T> listAll(Orderings orderings) throws ServiceException;
+
+    /**
+     * 查找所有的实体列表, 不分页<br>
+     * 注意: 默认查询条件由entityFieldFillExecutor添加, 只查有效项<br>
+     * SELECT {columnNames} FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE A.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ORDER BY {orderByConditions}
+     * 
+     * @param fields 查询的字段: 全部字段传入Fields.ALL, 指定字段传入IncludeFields对象, 排除字段传入ExcludeFields对象<br>
+     *            如: new IncludeFields("a.id"), 将会查询A.ID<br>
+     *            如: new IncludeFields("id"), 将会查询A.ID,B.ID<br>
+     * @param orderings 排序字段, 不需要排序时应传入Orderings.NONE
+     * @return 列表数据
+     */
+    List<T> listAll(Fields fields, Orderings orderings) throws ServiceException;
+
+    /**
+     * 根据条件分页查询实体列表<br>
+     * 注意: 默认查询条件由entityFieldFillExecutor添加, 只查有效项<br>
+     * <br>
+     * SELECT COUNT(*) FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0<br>
+     * SELECT {columnNames} FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ORDER BY {orderByConditions}
+     * 
+     * @param where 查询条件, 如果没有查询条件应传入DbWhere.NONE
+     * @param orderings 排序字段, 不需要排序时应传入Orderings.NONE
+     * @return 列表数据
+     */
+    List<T> list(DbWhere where, Orderings orderings) throws ServiceException;
 
     /**
      * 根据条件分页查询实体列表<br>
@@ -86,6 +153,52 @@ public interface JoinQueryer<T> {
      * @return 列表数据
      */
     PageList<T> list(DbWhere where, OrderPaging odpg) throws ServiceException;
+
+    /**
+     * 根据条件分页查询实体列表<br>
+     * 注意: 默认查询条件由entityFieldFillExecutor添加, 只查有效项<br>
+     * <br>
+     * SELECT COUNT(*) FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0<br>
+     * SELECT {columnNames} FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ORDER BY {orderByConditions}
+     * 
+     * @param fields 查询的字段: 全部字段传入Fields.ALL, 指定字段传入IncludeFields对象, 排除字段传入ExcludeFields对象<br>
+     *            如: new IncludeFields("a.id"), 将会查询A.ID<br>
+     *            如: new IncludeFields("id"), 将会查询A.ID,B.ID<br>
+     * @param where 查询条件, 如果没有查询条件应传入DbWhere.NONE
+     * @param orderings 排序条件, 不需要排序时应传入Orderings.NONE
+     * @return 列表数据
+     */
+    List<T> list(Fields fields, DbWhere where, Orderings orderings) throws ServiceException;
+
+    /**
+     * 根据条件分页查询实体列表<br>
+     * 注意: 默认查询条件由entityFieldFillExecutor添加, 只查有效项<br>
+     * <br>
+     * SELECT COUNT(*) FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0<br>
+     * SELECT {columnNames} FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ORDER BY {orderByConditions}
+     * 
+     * @param fields 查询的字段: 全部字段传入Fields.ALL, 指定字段传入IncludeFields对象, 排除字段传入ExcludeFields对象<br>
+     *            如: new IncludeFields("a.id"), 将会查询A.ID<br>
+     *            如: new IncludeFields("id"), 将会查询A.ID,B.ID<br>
+     * @param where 查询条件, 如果没有查询条件应传入DbWhere.NONE
+     * @param odpg 分页/排序条件, 不需要分页也不需要排序时应传入OrderPaging.NONE
+     * @return 列表数据
+     */
+    PageList<T> list(Fields fields, DbWhere where, OrderPaging odpg) throws ServiceException;
 
     /**
      * 根据条件统计实体数量<br>
@@ -144,10 +257,31 @@ public interface JoinQueryer<T> {
      * @param fieldName 指定字段名
      * @param distinct 是否去重
      * @param where 查询条件, 如果没有查询条件应传入DbWhere.NONE
+     * @param orderings 排序条件, 不需要排序时应传入Orderings.NONE
+     * @param valueClazz 字段值类型
+     * @return 字段的值列表
+     */
+    <V> List<V> listFieldValues(String fieldName, boolean distinct, DbWhere where, Orderings orderings,
+            Class<V> valueClazz) throws ServiceException;
+
+    /**
+     * 根据条件查询某个字段的值列表<br>
+     * 注意: 默认查询条件由entityFieldFillExecutor添加, 只查有效项<br>
+     * <br>
+     * SELECT {columnName} FROM {tableNameA} A<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;INNER JOIN {tableNameB} B<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ON A.DATA_ID=B.ID AND B.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;WHERE {whereConditions} AND A.DATA_STATE=0<br>
+     * &nbsp;&nbsp;&nbsp;&nbsp;ORDER BY {orderByConditions}
+     * 
+     * @param fieldName 指定字段名
+     * @param distinct 是否去重
+     * @param where 查询条件, 如果没有查询条件应传入DbWhere.NONE
      * @param odpg 分页/排序条件, 不需要分页也不需要排序时应传入OrderPaging.NONE
      * @param valueClazz 字段值类型
      * @return 字段的值列表
      */
     <V> PageList<V> listFieldValues(String fieldName, boolean distinct, DbWhere where, OrderPaging odpg,
             Class<V> valueClazz) throws ServiceException;
+
 }
