d68c5aadc1f8e4ecad0ffadab8455a698c181f0d
zhaohuihua <zhaohuihua@126.com>
2020-09-20 12:35:41 +0800

    增加配置项

--- jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/base/DatabaseInitiator.java
+++ jdbc-test/src/test/java/com/gitee/qdbp/jdbc/test/base/DatabaseInitiator.java
@@ -2,6 +2,7 @@ package com.gitee.qdbp.jdbc.test.base;
 
 import javax.annotation.PostConstruct;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.test.context.testng.AbstractTestNGSpringContextTests;
 import com.gitee.qdbp.jdbc.api.QdbcBoot;
@@ -10,11 +11,17 @@ import com.gitee.qdbp.jdbc.model.DbType;
 @Service
 public class DatabaseInitiator extends AbstractTestNGSpringContextTests {
 
+    @Value("#{setting['database.init.enabled']}")
+    private boolean enabled;
     @Autowired
     private QdbcBoot qdbcBoot;
 
     @PostConstruct
     public void init() {
+        if (!enabled) {
+            return;
+        }
+
         DbType dbType = qdbcBoot.getSqlDialect().getDbVersion().getDbType();
         String path = "settings/dbinit/create.tables." + dbType.name().toLowerCase() + ".sql";
         qdbcBoot.getSqlBufferJdbcOperations().executeSqlScript(path);
