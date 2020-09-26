dc2060346dda6c6a019e001faed851847e226d56
zhaohuihua <zhaohuihua@126.com>
2020-09-26 23:37:36 +0800

    ScanResource测试

---
+++ jdbc-test/src/test/java/com/gitee/qdbp/jdbc/base/ScanFileResourceTest.java
@@ -0,0 +1,30 @@
+package com.gitee.qdbp.jdbc.base;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import com.gitee.qdbp.tools.files.PathTools;
+
+public class ScanFileResourceTest {
+
+    public static void main(String[] args) throws IOException {
+        test("logback.xml");
+        System.out.println();
+        test("setting.properties");
+        System.out.println();
+        test("settings/tags/taglib.txt");
+        System.out.println();
+        test("support/http/resources/api.html");
+        System.out.println();
+        test("META-INF/MANIFEST.MF");
+    }
+
+    private static void test(String resource) {
+        System.out.println("<<" + resource + ">>");
+        List<URL> urls = PathTools.scanResources(resource);
+        for (URL url : urls) {
+            System.out.println(url);
+        }
+    }
+
+}
---
+++ jdbc-test/src/test/java/com/gitee/qdbp/jdbc/base/ScanFolderResourceTest.java
@@ -0,0 +1,42 @@
+package com.gitee.qdbp.jdbc.base;
+
+import java.io.IOException;
+import java.net.URL;
+import java.util.List;
+import com.gitee.qdbp.able.matches.FileMatcher;
+import com.gitee.qdbp.able.matches.StringMatcher.LogicType;
+import com.gitee.qdbp.able.matches.WrapFileMatcher;
+import com.gitee.qdbp.tools.files.PathTools;
+
+public class ScanFolderResourceTest {
+
+    public static void main(String[] args) throws IOException {
+        test("settings/", "*.xml");
+        System.out.println();
+        test("settings/", "*.properties");
+        System.out.println();
+        test("settings/", "*.txt");
+        System.out.println();
+        test("settings/", new WrapFileMatcher(LogicType.OR, "name:ant:*.xml", "name:ant:*.properties"));
+        System.out.println();
+        test("support/http/resources/", "web*.html");
+        System.out.println();
+        test("support/", "wall.*");
+    }
+
+    private static void test(String folder, String filter) {
+        System.out.println("<<" + folder + ">>    " + filter);
+        List<URL> urls = PathTools.scanResources(folder, filter);
+        for (URL url : urls) {
+            System.out.println(url);
+        }
+    }
+
+    private static void test(String folder, FileMatcher matcher) {
+        System.out.println("<<" + folder + ">>    " + matcher.toString());
+        List<URL> urls = PathTools.scanResources(folder, matcher);
+        for (URL url : urls) {
+            System.out.println(url);
+        }
+    }
+}
