646edbe35f3fbe2f77afc391098533bcdc5500ed
zhaohuihua <zhaohuihua@126.com>
2020-10-21 23:15:55 +0800

    优化context变量取值逻辑

--- jdbc-core/src/main/java/com/gitee/qdbp/jdbc/sql/parse/SqlBufferContext.java
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/sql/parse/SqlBufferContext.java
@@ -3,9 +3,11 @@ package com.gitee.qdbp.jdbc.sql.parse;
 import com.gitee.qdbp.jdbc.plugins.SqlDialect;
 import com.gitee.qdbp.jdbc.sql.SqlBuffer;
 import com.gitee.qdbp.jdbc.sql.SqlBuilder;
+import com.gitee.qdbp.jdbc.utils.DbTools;
 import com.gitee.qdbp.staticize.exception.TagException;
 import com.gitee.qdbp.staticize.publish.BaseContext;
 import com.gitee.qdbp.staticize.utils.OgnlTools;
+import com.gitee.qdbp.tools.utils.StringTools;
 
 /**
  * 环境变量管理类
@@ -42,20 +44,31 @@ class SqlBufferContext extends BaseContext {
      */
     @Override
     public Object doGetValue(String prefix, String expression) throws TagException {
-        Object value = OgnlTools.getValue(stack(), expression);
-        if (value == null) {
-            return null;
-        }
-
         if ("#".equals(prefix)) { // 预编译参数
-            return new SqlBuffer().addVariable(value);
+            Object value = OgnlTools.getValue(stack(), expression);
+            return value == null ? null : new SqlBuffer().addVariable(value);
         } else if ("$".equals(prefix)) { // 拼写式参数
-            if (value instanceof SqlBuffer) {
-                return ((SqlBuffer) value).getExecutableSqlString(dialect);
-            } else if (value instanceof SqlBuilder) {
-                return ((SqlBuilder) value).out().getExecutableSqlString(dialect);
+            if (expression.trim().startsWith("sql:")) {
+                String exp = StringTools.removePrefix(expression.trim(), "sql:");
+                Object value = OgnlTools.getValue(stack(), exp);
+                if (value instanceof SqlBuffer) {
+                    return ((SqlBuffer) value).getExecutableSqlString(dialect);
+                } else if (value instanceof SqlBuilder) {
+                    return ((SqlBuilder) value).out().getExecutableSqlString(dialect);
+                } else {
+                    return DbTools.variableToString(value, dialect);
+                }
             } else {
-                return value;
+                Object value = OgnlTools.getValue(stack(), expression);
+                if (value == null) {
+                    return null;
+                } else if (value instanceof SqlBuffer) {
+                    return ((SqlBuffer) value).getExecutableSqlString(dialect);
+                } else if (value instanceof SqlBuilder) {
+                    return ((SqlBuilder) value).out().getExecutableSqlString(dialect);
+                } else {
+                    return value;
+                }
             }
         } else {
             throw new TagException("表达式前缀有误");
--- jdbc-core/src/main/java/com/gitee/qdbp/jdbc/sql/parse/SqlCachingWriter.java
+++ jdbc-core/src/main/java/com/gitee/qdbp/jdbc/sql/parse/SqlCachingWriter.java
@@ -1,9 +1,14 @@
 package com.gitee.qdbp.jdbc.sql.parse;
 
 import java.io.IOException;
+import java.util.Collection;
+import java.util.Date;
+import java.util.List;
 import com.gitee.qdbp.jdbc.sql.SqlBuffer;
 import com.gitee.qdbp.jdbc.sql.SqlBuilder;
 import com.gitee.qdbp.staticize.common.IWriter;
+import com.gitee.qdbp.tools.utils.ConvertTools;
+import com.gitee.qdbp.tools.utils.DateTools;
 
 /**
  * 缓存SQL的Writer
@@ -23,14 +28,21 @@ public class SqlCachingWriter implements IWriter {
         }
         if (value instanceof String) {
             this.caching.append((String) value);
-        } else if (value instanceof Character) {
-            this.caching.append((Character) value);
         } else if (value instanceof SqlBuffer) {
             this.caching.append((SqlBuffer) value);
         } else if (value instanceof SqlBuilder) {
             this.caching.append(((SqlBuilder) value).out());
+        } else if (value instanceof Character) {
+            this.caching.append((Character) value);
+        } else if (value instanceof CharSequence) {
+            this.caching.append(value.toString());
+        } else if (value instanceof Date) {
+            this.caching.append(DateTools.toNormativeString((Date) value));
+        } else if (value instanceof Collection || value.getClass().isArray()) {
+            List<Object> values = ConvertTools.parseList(value);
+            this.caching.append(ConvertTools.joinToString(values, ',', false));
         } else {
-            throw new IllegalArgumentException("UnsupportedArgumentType: " + value.getClass());
+            this.caching.append(value.toString());
         }
     }
 
